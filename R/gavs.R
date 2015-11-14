18e942bbf7e685d1108220bff0b1432dbb83240d
Alexander Meyer <meyer.alexander@gmail.com>
2015-11-14 21:12:57 +0100

    add roxygen comments

--- R/gavs.R
+++ R/gavs.R
@@ -5,10 +5,31 @@
 # European Journal of Cardio-Thoracic Surgery 43(5), 971–7.
 # http://doi.org/10.1093/ejcts/ezt114
 
+#' Implements the GAVS model
+#'
+#' @param age_grp Age groups (NULL=ref,ref="18_65", "66_70", "71_75", "76_80", "81_85", ">85")
+#' @param female boolean string
+#' @param bmi_grp BMI groups (NULL=ref, ref="22_35", "<22", ">35")
+#' @param mi_within_3w boolean string
+#' @param critical boolean string
+#' @param pht boolean string
+#' @param no_sinus_rhythm boolean string
+#' @param lvef_grp LVEF groups (NULL=ref, ref="good_or_>50", "medium_or_30-50", "poor_<30"
+#' @param endocarditis boolean string
+#' @param redo boolean string
+#' @param aterial_vessel_diseae boolean string
+#' @param copd boolean string
+#' @param preop_dialysis_or_rf boolean string
+#' @param emergency boolean string
+#'
+#' @return the German aortic valve score expected probability of in-hospital-mortality
+#' @export
+#'
+#' @examples
 gavs <- function(age_grp = NULL,
                  female = NULL,
                  bmi_grp = NULL,
-                 nyha4 = NULL,
+                 nyhaIV = NULL,
                  mi_within_3w = NULL,
                  critical = NULL,
                  pht = NULL,
@@ -24,19 +45,61 @@ gavs <- function(age_grp = NULL,
   const <- -5.504
   logOdds <- const
 
+  if (!is.null(NYHA)) {
+    if (NYHA == 'II') {
+      logOdds <- logOdds + 0.1070545
+    } else if (NYHA == 'III') {
+      logOdds <- logOdds + 0.2958358
+    } else if (NYHA == 'IV') {
+      logOdds <- logOdds + 0.5597929
+    }
+  }
+
   pred_mort <- exp(logOdds) / (1 + exp(logOdds))
   return(round(pred_mort, digits = 4))
 }
 
 
+#' Title
+#'
+#' @param age
+#' @param sex
+#' @param weight_kg
+#' @param height_cm
+#' @param bmi
+#' @param nyhaIV
+#' @param mi_within_3w
+#' @param pht
+#' @param no_sr
+#' @param lvef
+#' @param lvef_grp
+#' @param endocarditis
+#' @param redo
+#' @param aterial_vessel_diseae
+#' @param copd
+#' @param preop_dialysis_or_rf
+#'
+#' @return
+#' @export
+#'
+#' @examples
 calc_gavs <- function(age = NULL,
                       sex = NULL,
                       weight_kg = NULL,
                       height_cm = NULL,
                       bmi = weight_kg/((height_cm/100)^2),
-                      nyha = NULL,
-                      mi_3_weeks,
-
+                      nyhaIV = NULL,
+                      mi_within_3w = NULL,
+                      pht = NULL,
+                      no_sr = NULL,
+                      lvef = NULL,
+                      lvef_grp = NULL,
+                      endocarditis = NULL,
+                      redo = NULL,
+                      aterial_vessel_diseae = NULL,
+                      copd = NULL,
+                      preop_dialysis_or_rf = NULL,
+                      emergency = NULL
                       ) {
 
 }
